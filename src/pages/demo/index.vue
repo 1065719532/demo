11e23391fd98f310f3ee853e350d7a33ffadaa71
前端-李蓬宇 <1065719532@163.com>
2019-02-23 10:33:05 -0800

    目录

---
+++ src/pages/demo/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="aa">aaaaaa</section>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+<style lang="less">
+  @import "~/assets/css/demo/index.less";
+</style>
